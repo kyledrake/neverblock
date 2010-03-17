6e73add2ad54a7fe8f0a5f9cc89ac1798011961c
Dane Jensen <dane.jensen@gmail.com>
2010-03-17 16:58:52 -0700

    quick hack to get Net::BufferedIO working w/ OpenSSL

--- lib/neverblock/net/buffered_io.rb
+++ lib/neverblock/net/buffered_io.rb
@@ -6,17 +6,27 @@
 
 module Net
   class BufferedIO
-    
+
     def rbuf_fill
       # todo: handle timeout
       begin
         @rbuf << @io.read_nonblock(BUFSIZE)
       rescue IO::WaitReadable => e
-        NB.wait(:read,self.io)
+        if self.io.kind_of? OpenSSL::SSL::SSLSocket
+          io = self.io.io
+        else
+          io = self.io
+        end
+        NB.wait(:read,io)
         retry
-       rescue IO::WaitWritable => e
-         NB.wait(:write,self.io)
-         retry
+      rescue IO::WaitWritable => e
+        if self.io.kind_of? OpenSSL::SSL::SSLSocket
+          io = self.io.io
+        else
+          io = self.io
+        end
+        NB.wait(:write,io)
+        retry
       end
     end
     
