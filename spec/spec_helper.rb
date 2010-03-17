302a7b5ce0960343ec7263b3770c97da56325557
Dane Jensen <dane.jensen@gmail.com>
2010-03-17 12:52:59 -0700

    initial TestServer class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,17 @@
 require_relative "../lib/neverblock"
+
+class TestServer
+  attr_accessor :r, :w, :status
+  
+  def initialize server
+    path = File.expand_path("../servers/#{server}.rb",__FILE__)
+    puts path
+    @r, @w = IO.popen "ruby #{path}"
+    @status = $?
+  end
+
+  def stop
+    Process.kill @status.pid
+  end
+
+end
