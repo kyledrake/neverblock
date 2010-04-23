1fc782c3666104f438f2535e484a95c77e131a0b
Jake Douglas <jakecdouglas@gmail.com>
2010-04-23 11:12:44 -0700

    timeout in Net::BufferedIO#rbuf_fill

--- lib/neverblock/net/buffered_io.rb
+++ lib/neverblock/net/buffered_io.rb
@@ -1,16 +1,14 @@
 # Monkeypatch Net::BufferedIO#rbuf_fill
 # use NeverBlock to wait on IO, not IO.select
-#
-# Doesn't do read timeouts yet, but... whatever. I don't care nearly as much
-# if the timeout isn't blocking.
 
 module Net
   class BufferedIO
 
     def rbuf_fill
-      # todo: handle timeout
       begin
-        @rbuf << @io.read_nonblock(BUFSIZE)
+        timeout(@read_timeout) {
+          @rbuf << @io.read_nonblock(BUFSIZE)
+        }
       rescue IO::WaitReadable, Errno::EAGAIN, Errno::EWOULDBLOCK, Errno::EINTR => e
         NB.wait(:read, @io)
         retry
