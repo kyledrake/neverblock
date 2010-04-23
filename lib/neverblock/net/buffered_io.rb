464f5f52e70d70ef66336205a367372587f70adf
Jake Douglas <jakecdouglas@gmail.com>
2010-04-23 11:43:58 -0700

    timeout needs to encapsulate the whole method

--- lib/neverblock/net/buffered_io.rb
+++ lib/neverblock/net/buffered_io.rb
@@ -5,17 +5,17 @@ module Net
   class BufferedIO
 
     def rbuf_fill
-      begin
-        timeout(@read_timeout) {
+      timeout (@read_timeout) {
+        begin
           @rbuf << @io.read_nonblock(BUFSIZE)
-        }
-      rescue IO::WaitReadable, Errno::EAGAIN, Errno::EWOULDBLOCK, Errno::EINTR => e
-        NB.wait(:read, @io)
-        retry
-      rescue IO::WaitWritable => e
-        NB.wait(:write, @io)
-        retry
-      end
+        rescue IO::WaitReadable, Errno::EAGAIN, Errno::EWOULDBLOCK, Errno::EINTR => e
+          NB.wait(:read, @io)
+          retry
+        rescue IO::WaitWritable => e
+          NB.wait(:write, @io)
+          retry
+        end
+      }
     end
     
   end 
